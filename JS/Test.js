187e603efe678e52b6b10b3b75263cc5632332d8
JeongSeo-Lee <wjdtj35@gmail.com>
2021-06-05 21:24:05 +0900

    SamSung laptop cafe 2021 6/5 9:23

--- JS/Test.js
+++ JS/Test.js
@@ -15,30 +15,66 @@
 //     }
 // }
 
-var ii = 1;
-for(var i=9; i>=1;i -= 2){
-    if(!(i == 9)){
-        console.log(' '.repeat(9-i -ii ) + '*'.repeat(i) + ' '.repeat(9-i -ii ) );
-        ii++;
-}else{
-    console.log(' '.repeat(9-i ) + '*'.repeat(i) + ' '.repeat(9-i) );
+// var ii = 1;
+// for(var i=9; i>=1;i -= 2){
+//     if(!(i == 9)){
+//         console.log(' '.repeat(9-i -ii ) + '*'.repeat(i) + ' '.repeat(9-i -ii ) );
+//         ii++;
+// }else{
+//     console.log(' '.repeat(9-i ) + '*'.repeat(i) + ' '.repeat(9-i) );
 
-    } 
+//     } 
+// }
+
+
+// var ii = -1;
+// for(var i = 1; i <=5; i++){
+//     if(i<=3){
+//         ii += 2;
+//         console.log(' '.repeat((5 -ii)/2) + '*'.repeat(ii));
+//         // console.log(' '.repeat(9-i/2) + '*'.repeat(i));
+//     } else {
+//         ii -=2;
+//         console.log(' '.repeat((5 -ii)/2) + '*'.repeat(ii));
+//     }
+// }
+
+var 바디 = document.body;
+
+var arr = [1,2,3,4,5,6,7,8,9];
+var result = [];
+
+for(var i = 0; i < 4; i++ ){
+    var tempNum = arr.splice(Math.floor(Math.random()*(9 - i)) , 1)[0];
+    result.push(tempNum);
 }
+console.log(`>> ${result}`);
 
 
-var ii = -1;
-for(var i = 1; i <=5; i++){
-    if(i<=3){
-        ii += 2;
-        console.log(' '.repeat((5 -ii)/2) + '*'.repeat(ii));
-        // console.log(' '.repeat(9-i/2) + '*'.repeat(i));
+var 결과 = document.createElement('h1');
+바디.append(결과);
+var 폼 = document.createElement('form');
+바디.append(폼);
+var inputTag = document.createElement('input');
+폼.append(inputTag);
+inputTag.type ='text';
+inputTag.maxLength = 4;
+var btn = document.createElement('button');
+btn.textContent = '입력';
+폼.append(btn);
+console.log(폼);
+
+폼.addEventListener('submit', function 비동기(e){  // 언제실행될 지 모름
+    e.preventDefault();  // 이벤트 멈추기
+    var 답 ="";
+    답 = inputTag.value;
+    // console.log(`타입 : ${typeof 답.substr(0,1) }, 타입 : ${ typeof result[0]}`)
+    result.join('');
+
+    if(parseInt(답) === parseInt(result.join(''))){
+        console.log('정답');
     } else {
-        ii -=2;
-        console.log(' '.repeat((5 -ii)/2) + '*'.repeat(ii));
+        alert(`정답이 아닙니다.`)
     }
-
-
-
-}
+});
 
