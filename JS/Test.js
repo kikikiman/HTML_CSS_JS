437b757d567824d4f477ebb14547286e50e6906b
JeongSeo-Lee <wjdtj35@gmail.com>
2021-06-07 20:29:40 +0900

    NumberBaseBallGame_complete!

--- JS/Test.js
+++ JS/Test.js
@@ -39,17 +39,25 @@
 //     }
 // }
 
+
+
+/*
+ *  숫자 야구
+ */
 var 바디 = document.body;
+var arr;
+var result; 
 
-var arr = [1,2,3,4,5,6,7,8,9];
-var result = [];
-
-for(var i = 0; i < 4; i++ ){
-    var tempNum = arr.splice(Math.floor(Math.random()*(9 - i)) , 1)[0];
-    result.push(tempNum);
+function 숫자뽑기(){
+    arr = [1,2,3,4,5,6,7,8,9];
+    result = [];
+    for(var i = 0; i < 4; i++ ){
+        var tempNum = arr.splice(Math.floor(Math.random()*(9 - i)) , 1)[0];
+        result.push(tempNum);
+    }
 }
-console.log(`>> ${result}`);
-
+숫자뽑기();
+console.log('result[] : '+result);
 
 var 결과 = document.createElement('h1');
 바디.append(결과);
@@ -62,19 +70,46 @@ inputTag.maxLength = 4;
 var btn = document.createElement('button');
 btn.textContent = '입력';
 폼.append(btn);
-console.log(폼);
 
+
+var 틀린횟수 = 0;
 폼.addEventListener('submit', function 비동기(e){  // 언제실행될 지 모름
     e.preventDefault();  // 이벤트 멈추기
-    var 답 ="";
-    답 = inputTag.value;
-    // console.log(`타입 : ${typeof 답.substr(0,1) }, 타입 : ${ typeof result[0]}`)
-    result.join('');
-
-    if(parseInt(답) === parseInt(result.join(''))){
-        console.log('정답');
+    var 답 = inputTag.value;
+    if(답 === result.join('')){
+        결과.textContent = '홈런!!!';
+        inputTag.value = '';
+        inputTag.focus();
+        
+        숫자뽑기()
+      
+        틀린횟수 = 0;
     } else {
-        alert(`정답이 아닙니다.`)
+        var resultArr = 답.split('');
+        var strike = 0;
+        var ball = 0;
+        틀린횟수++;
+        if(틀린횟수 >4){
+            결과.textContent = '실패 10번 넘게 틀려서 정답 공개, 정답은 : ' + result.join(',');
+            inputTag.value = '';
+            inputTag.focus();
+
+            숫자뽑기()
+            틀린횟수 = 0;
+        } else{
+            for(var i = 0; i <= 3; i++){
+                if(Number(resultArr[i]) === result[i]){
+                    strike++;
+                } else if(result.indexOf(Number(resultArr[i])) > -1){
+                    console.log('볼');
+                    ball++;
+                }
+            }
+            결과.textContent = strike + ' Strike, ' + ball + ' ball';
+            inputTag.value = '';
+            inputTag.focus();
+        }
+
     }
 });
 
